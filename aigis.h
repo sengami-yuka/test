360f27eef47b5608368f011ab0ceb1ffee91b2ab
sengami-yuka <sengami.yuka@gmail.com>
2015-10-20 15:40:39 -0700

    Update and rename aigis.cpp to aigis.h

---
+++ aigis.h
@@ -0,0 +1,27 @@
+class Unit {
+
+
+
+};
+
+
+class Soldier {
+  public:
+    int attack();
+    int defence();
+    int hp();
+    int cost();
+    int block();
+    bool cc();
+    int affection();
+    int mr();
+    Skill skill();
+  private:
+    
+};
+
+
+class Archer {
+
+
+};
